f354124ae1b64307908317ae79d3e3e306da8f9f
fahad <fuddin@sightlineinnovation.com>
2021-08-05 00:35:52 -0400

    first app

---
+++ firstapp/src/index.tsx
@@ -0,0 +1,13 @@
+import  ReactDOM from "react-dom";
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hi there</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
